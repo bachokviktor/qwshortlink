cf1b112591944104fb6a96f6eb916a0dffe71e87
Bachok Viktor <bachok.viktor@gmail.com>
2026-04-04 14:15:56 +0300

    Set up pytest

---
+++ backend/tests/test_models.py
@@ -0,0 +1,14 @@
+import uuid
+import pytest
+
+
+@pytest.mark.django_db
+class TestUsers:
+    def test_create_user(self, django_user_model):
+        user = django_user_model.objects.create_user(
+            username="testuser",
+            password="x5AXFqw7"
+        )
+
+        assert isinstance(user.id, uuid.UUID)
+        assert django_user_model.objects.count() == 1
