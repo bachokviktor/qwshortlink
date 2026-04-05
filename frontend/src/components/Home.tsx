73e8001fbae70444023df4e7db39dd13150ae7bc
Bachok Viktor <bachok.viktor@gmail.com>
2026-04-05 22:03:45 +0300

    Add basic styles

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,6 +1,10 @@
 function Home() {
   return (
-    <h2>This is a home page</h2>
+    <div className="centered-container">
+      <img alt="Logo" src="https://placehold.co/256" />
+      <h1>QWShortLink</h1>
+      <p>URL Shortener built with Django REST Framework and React</p>
+    </div>
   )
 }
 
--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,9 +1,12 @@
+import {Link} from "react-router"
+
 function NotFound() {
   return (
-    <>
+    <div className="centered-container">
       <h2>404 Not Found</h2>
       <p>This page does not exist</p>
-    </>
+      <Link to="/">Home</Link>
+    </div>
   )
 }
 
