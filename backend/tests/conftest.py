34326295a14aab65334306101f8ae53b7890bf41
Bachok Viktor <bachok.viktor@gmail.com>
2026-04-06 14:46:45 +0300

    Use DummyCache backend in tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,6 +2,15 @@ import pytest
 from rest_framework.test import APIClient
 
 
+@pytest.fixture(autouse=True)
+def use_dummy_cache_backend(settings):
+    settings.CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        }
+    }
+
+
 @pytest.fixture
 def django_test_user(db, django_user_model):
     """
