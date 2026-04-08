b5f516899bcaa6a51a75827cb15b364d40462d8f
Bachok Viktor <bachok.viktor@gmail.com>
2026-04-08 22:22:57 +0300

    Define basic profile page structure

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -5,8 +5,21 @@ function Profile() {
   const auth = useContext(AuthContext)
 
   return (
-    <div>
-      <h2>Welcome, {auth.user?.username}!</h2>
+    <div className="container">
+      <div className="user-container">
+	<h2>{auth.user?.username} {auth.user?.is_staff && "(staff)"}</h2>
+	<p>First name: </p>
+	<p>Last name: </p>
+	<p>Email: </p>
+	<button>Edit profile</button>
+	<button className="btn-danger">Delete user</button>
+      </div>
+      <div className="links-container">
+	<button>Add link</button>
+	<div className="link-card">
+	  <p>https://djangoproject.com/</p>
+	</div>
+      </div>
     </div>
   )
 }
