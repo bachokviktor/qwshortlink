3667cc92cfa757eaa267fe97001db9817a74e91b
Bachok Viktor <bachok.viktor@gmail.com>
2026-04-19 18:52:05 +0300

    Update Django settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -35,10 +35,14 @@ SECRET_KEY = os.getenv("DJ_SECRET_KEY", "fake_secret_key")
 DEBUG = True if os.getenv("DJ_DEBUG") == "t" else False
 
 ALLOWED_HOSTS = [
-    "*",
+    os.getenv("ALLOWED_HOSTS"),
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    os.getenv("CORS_ORIGINS")
+]
 
 CORS_EXPOSE_HEADERS = [
     "retry-after",
