61785097432806ba2eac40c723c50b6211a59453
Bachok Viktor <bachok.viktor@gmail.com>
2026-05-31 21:40:51 +0300

    Fix username min length

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,18 @@
+from rest_framework import serializers
 from dj_rest_auth.serializers import UserDetailsSerializer
+from allauth.account import app_settings as allauth_account_settings
+from allauth.utils import get_username_max_length
 
 
 class UserSerializer(UserDetailsSerializer):
     """
     This serializer is used to retrieve or update user data.
     """
+    username = serializers.CharField(
+        max_length=get_username_max_length(),
+        min_length=allauth_account_settings.USERNAME_MIN_LENGTH,
+    )
+
     class Meta(UserDetailsSerializer.Meta):
         fields = [
             "pk",
